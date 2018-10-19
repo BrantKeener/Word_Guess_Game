8329a4878bac5aa82870c251eeee19062e2a079e
unknown <brant_keener@yahoo.com>
2018-10-18 21:09:04 -0600

    javascript handling key presses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,36 +2,55 @@
 var correct = 0;
 var incorrect = 0;
 var words = ["transfusion", 
-    "Red Blood Cell", 
+    "plasma", 
     "antibody", 
     "antigen"];
 
-var testWord = ["box"]
 var word = document.getElementById("word");
 
 // Click event that fires a function that itself passes arguments. Click cannot pass arguments.
+
 document.getElementById("start").addEventListener("click", function() {
-    wordSplit(testWord, 0);
+    var wordChoice = Math.floor(Math.random() * words.length);
+    wordSplit(words, wordChoice);
 });
 
-// This will split whatever word is passed in from the array into another array that we can use. We need to pass letters out of this funciton.
+// This will split whatever word is passed in from the array into another array that we can use.
+
 function wordSplit(wordArray, choice){
-    beginWord = wordArray[choice];
-    console.log(beginWord);
+    var beginWord = wordArray[choice];
     var letters = beginWord.split("");
-    console.log(letters);
     wordSet(letters);
+    return letters;
 }
 
-// This should set up three paragraphs with underlines.
+// This should set up three paragraphs with underlines, add underlines, and set class.
+
 function wordSet(letters) {
     var paraDiv = document.getElementById("word");
-    letters.forEach(letter => {
+    paraDiv.innerHTML = "";
+    for(i = 0; i< letters.length; i++) {
         var wordPar = document.createElement("p");
         wordPar.textContent = "_ ";
         wordPar.classList.add("word_class");
-        console.log(wordPar);
+        wordPar.setAttribute("id", "letter" + i);
         paraDiv.appendChild(wordPar);
-});
+        console.log(wordPar);
 };
-// Next, letters will be used to build the necessary underlined spaces.
+};
+
+// This function will handle button presses, update the html, or give an alert.
+
+function wordCheck(letters) {
+    document.addEventListener("keydown", function() {
+        var pressedKey;
+        pressedKey = event.key;
+        console.log(pressedKey);
+        console.log(event.key);
+        for(i=0; i < letters.length; i++) {
+            if(pressedKey === letters[i]){
+                document.getElementById("letter" + i).textContent = pressedKey;
+            }
+        }
+        });
+    };
