4959f28a5b90b0c4bb2551f06b20ec28a06763e2
unknown <brant_keener@yahoo.com>
2018-10-19 19:52:38 -0600

    Javascript for correct guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,57 +1,75 @@
 
 var correct = 0;
 var incorrect = 0;
-var words = ["transfusion", 
+var words1 = ["transfusion", 
     "plasma", 
     "antibody", 
-    "antigen"];
+    "antigen",
+    "platelets",
+    "titer"];
 
+var words2 = [];
 var word = document.getElementById("word");
+var letters = [];
 
-// Click event that fires a function that itself passes arguments. Click cannot pass arguments.
+// This section of code handles the word setting, and the word evaluating.
+// I added all of the array switching stuff because I don't like when a random generator generates the same word over and over.
 
 document.getElementById("start").addEventListener("click", function() {
-    var wordChoice = Math.floor(Math.random() * words.length);
-    wordSplit(words, wordChoice);
+    if(!words1.length) {
+        words1 = words2;
+        words2 = [];
+    };
+    var wordChoice = Math.floor(Math.random() * words1.length);
+    wordSplit(words1, wordChoice);
 });
-
-// This will split whatever word is passed in from the array into another array that we can use.
 
 function wordSplit(wordArray, choice){
     var beginWord = wordArray[choice];
-    var letters = beginWord.split("");
+    var currentWord = beginWord.split("");
+    letters = currentWord;
     wordSet(letters);
-    wordCheck(letters);
-    // console.log(letters);
+    if(words1 !== -1) {
+        words2.push(words1[choice]);
+        words1.splice(choice, 1);
+        console.log(words1);
+        console.log(words2);
+    };
 }
 
-// This should set up three paragraphs with underlines, add underlines, and set class.
+function wordSet(creativeWord) {
+    var setDiv = document.getElementById("word");
+    setDiv.innerHTML = "";
+    for(i = 0; i< creativeWord.length; i++) {
+        var wordPar = document.createElement("p");
+        wordPar.textContent = "_ ";
+        wordPar.classList.add("word_class");
+        wordPar.setAttribute("id", "letter" + i);
+        setDiv.appendChild(wordPar);
+};
+};
 
-function wordSet(letters) {
+document.addEventListener("keydown", function() {
+    wordCheck(event.key);
+});
+
+
+function wordCheck(key) {
+    for(i=0; i < letters.length; i++) {
+        if(key === letters[i]){  
+            document.getElementById("letter" + i).textContent = key;
+        }
+    }
+};
+
+function wordSet(creativeWord) {
     var paraDiv = document.getElementById("word");
     paraDiv.innerHTML = "";
-    for(i = 0; i< letters.length; i++) {
+    for(i = 0; i< creativeWord.length; i++) {
         var wordPar = document.createElement("p");
         wordPar.textContent = "_ ";
         wordPar.classList.add("word_class");
         wordPar.setAttribute("id", "letter" + i);
         paraDiv.appendChild(wordPar);
-        // console.log(letters);
 };
 };
-
-// This function will handle button presses, update the html, or give an alert.
-
-function wordCheck(letters) {
-    document.addEventListener("keydown", function() {
-        console.log(letters);
-        var pressedKey;
-        pressedKey = event.key;
-        for(i=0; i < letters.length; i++) {
-            if(pressedKey === letters[i]){
-                // console.log(letters);
-                document.getElementById("letter" + i).textContent = pressedKey;
-            }
-        }
-        });
-    };
