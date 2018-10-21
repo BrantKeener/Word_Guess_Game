961ba828d93f8e69e7108eb376f2f9d473e47290
unknown <brant_keener@yahoo.com>
2018-10-20 20:28:24 -0600

    javascript that deals with wrong guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,11 +12,21 @@ let words2 = [];
 let word = document.getElementById("word");
 let letters = [];
 let chosenWord = "";
+let guessedWord = "";
+let chances = 6;
 
 // This section deals the word setting, and the word evaluating.
 // I added all of the array switching stuff because I don't like when a random generator generates the same word over and over.
 
 document.getElementById("start").addEventListener("click", function() {
+    roundStart();
+});
+
+function roundStart(){
+    guessedWord = "";
+    chosenWord = "";
+    chances = 6;
+    document.getElementById("remaining").textContent = "Guesses Remaining: " + chances;
     if(!words1.length) {
         words1 = words2;
         words2 = [];
@@ -24,7 +34,7 @@ document.getElementById("start").addEventListener("click", function() {
     let wordChoice = Math.floor(Math.random() * words1.length);
     chosenWord = words1[wordChoice];
     wordSplit(words1, wordChoice);
-});
+};
 
 function wordSplit(wordArray, choice){
     let beginWord = wordArray[choice];
@@ -41,7 +51,7 @@ function wordSet(creativeWord) {
     let setDiv = document.getElementById("word");
     setDiv.innerHTML = "";
     document.getElementById("guessedLetters").innerHTML="";
-    for(i = 0; i< creativeWord.length; i++) {
+    for(let i = 0; i< creativeWord.length; i++) {
         let setPar = document.createElement("p");
         setPar.textContent = "_ ";
         setPar.classList.add("word_class");
@@ -57,13 +67,15 @@ document.addEventListener("keydown", function() {
 
 
 function wordCheck(key) {
-    for(i=0; i < letters.length; i++) {
+    for(let i=0; i < letters.length; i++) {
         if(key === letters[i]){  
             document.getElementById("letter" + i).textContent = key;
-        } else {
-            guessedLetters(key);
         };
-    };
+        };
+    if(letters.indexOf(key) === -1) {
+        wrongGuess(key);
+        guessedLetters(key);
+    }
 };
 
 function guessedLetters(press) {
@@ -79,8 +91,26 @@ function guessedLetters(press) {
 
 function winCheck() {
     let guessedWord = document.getElementById("word").textContent;
-    var correctGuess = 0;
     if(guessedWord === chosenWord) {
-        document.getElementById("correct").innerHTML = "Patients Saved: " + ++correctGuess;
+        document.getElementById("correct").innerHTML = "Patients Saved: " + ++correct;
+        setTimeout(winMessage, 100);
     }
 };
+
+function winMessage() {
+    alert("Yep!")
+    roundStart();
+};
+
+// This section deals with wrong guesses
+
+function wrongGuess(press) {
+    var wrongLetters = document.getElementById("guessedLetters").textContent;
+    if(!wrongLetters.includes(press)) {
+        --chances;
+        document.getElementById("remaining").textContent = "Guesses Remaining: " + chances;
+    };
+};
+
+// This section handles the loss situation that occurs after you run out of guesses
+
